d84db1f929b34e3e8e4c7f4582f39e5645a0a029
Travis Brown <bergannation@gmail.com>
2021-04-15 18:27:16 -0500

    adding object for my loop

--- Assets/script.js
+++ Assets/script.js
@@ -1,63 +1,68 @@
 var today = dayjs();
 $("#currentDay").text(today.format("[Today is ] ddd - MMM D, YYYY, h:mma"));
 console.log(today);
-$(function () {
-  $.fn.currentTime = function () {
-    var now = dayjs().hour();
-    console.log(now);
-    if (now > 12) {
-      var newHour = now - 12;
-      console.log(newHour);
-      return newHour;
-    } else {
-      return now;
-    }
-  };
-  $.fn.currentTime();
-});
+
 var container = $(".container");
 var messageDiv = $(".message");
+$(document).ready(function () {
+  var dailySchedule = {
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: "",
+    7: "",
+    8: "",
+    9: "",
+    10: "",
+    11: "",
+    12: "",
+    13: "",
+    14: "",
+    15: "",
+    16: "",
+    17: "",
+    18: "",
+    19: "",
+    20: "",
+    21: "",
+    22: "",
+    23: "",
+    24: "",
+  };
+  console.log(dailySchedule);
+  localStorage.setItem("dailySchedule", JSON.stringify(dailySchedule));
 
-var dailySchedule = [
-  "09:00:",
-  "10:00:",
-  "11:00:",
-  "12:00:",
-  "1:00:",
-  "2:00:",
-  "3:00:",
-  "4:00:",
-  "5:00:",
-];
-localStorage.setItem("dailySchedule", JSON.stringify(dailySchedule));
+  for (var i = 0; i < 9; i++) {
+    var timeText = dailySchedule[i];
+    var sectionEl = $("<section>");
+    var divEl = $("<div>");
+    var spanEl = $("<span>");
+    var textareaEl = $("<textarea>");
+    var buttonEl = $("<button>");
+    var imgEl = $("<i>");
+    var messageEl = $("<article>");
 
-for (var i = 0; i < dailySchedule.length; i++) {
-  var timeText = dailySchedule[i];
-  var sectionEl = $("<section>");
-  var divEl = $("<div>");
-  var spanEl = $("<span>");
-  var textareaEl = $("<textarea>");
-  var buttonEl = $("<button>");
-  var imgEl = $("<i>");
-  var messageEl = $("<article>");
+    container.attr("class", "container-fluid");
 
-  container.attr("class", "container-fluid");
-
-  sectionEl.addClass("m-2 p-2 d-flex justify-content-center border-0");
-  container.append(sectionEl);
-  divEl.addClass("row time-block col-12 col-lg-9 m-0 p-0 shadow");
-  divEl.attr("data-label", timeText);
-  spanEl.addClass("hour col-1");
-  spanEl.text(timeText);
-  textareaEl.addClass("description col-10");
-  buttonEl.addClass("saveBtn btnEl btn-lg col-1");
-  imgEl.addClass("fa fa-save");
-  sectionEl.append(divEl);
-  divEl.append(spanEl);
-  divEl.append(textareaEl);
-  divEl.append(buttonEl);
-  buttonEl.append(imgEl);
-}
+    sectionEl.addClass("m-2 p-2 d-flex justify-content-center border-0");
+    container.append(sectionEl);
+    divEl.addClass("row time-block col-12 col-lg-9 m-0 p-0 shadow");
+    divEl.attr("data-label", timeText);
+    spanEl.addClass("hour col-1");
+    spanEl.text(timeText);
+    textareaEl.addClass("description col-10");
+    buttonEl.addClass("saveBtn btnEl btn-lg col-1");
+    imgEl.addClass("fa fa-save");
+    sectionEl.append(divEl);
+    divEl.append(spanEl);
+    divEl.append(textareaEl);
+    divEl.append(buttonEl);
+    buttonEl.append(imgEl);
+  }
+});
 
 var saveButton = $(".btnEl");
 var textInput = $("textarea");
@@ -101,3 +106,4 @@ function feedbackMessage(message) {
 // would you suggest making it an object instead of an array to have key value pairs?
 //currently my function to check timeblocks against current time isn't working
 //when i hit save, my message appears and disappears, but then i can't repeat it
+// line 78 on the display:none-get rid of style after it executes-message clear function
